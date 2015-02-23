5653bfa017569c997d4eb3a67186d993b63a10b0
Erik Gaal <git@erikgaal.nl>
2015-02-23 20:51:14 +0100

    Better option parsing for server

--- src/main/java/me/erikgaal/teatime/server/TeaTimeServer.java
+++ src/main/java/me/erikgaal/teatime/server/TeaTimeServer.java
@@ -4,6 +4,10 @@ import me.erikgaal.teatime.util.Logger;
 
 public class TeaTimeServer implements Runnable {
 
+    public static final String USAGE_STRING = "Usage: java server.TeaTimeServer [OPTIONS]\n" +
+            "\t--port\t\tlocal port number\n" +
+            "";
+
     private int serverPort;
 
     private Thread serverThread;
@@ -37,27 +41,34 @@ public class TeaTimeServer implements Runnable {
                 String key = args[i];
                 String value = i == args.length - 1 ? null : args[i + 1];
 
-                if (key.equals("--help")) {
-                    Logger.println("Usage: java teatime.server.TeaTimeServer [OPTION]...\n" +
-                            "Options:\n" +
-                            "\t--port\t\tlocal port number");
-                    System.exit(0);
-                } else if (key.equals("--port") && value != null) {
-                    try {
-                        port = Integer.parseInt(value);
-                    } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException("Invalid value " + value + " for key " + key);
+                if (key.startsWith("--")) {
+                    switch (key) {
+                        case "--help":
+                            Logger.println(USAGE_STRING);
+                            System.exit(0);
+                        case "--port":
+                            if (value != null && !value.startsWith("--")) {
+                                try {
+                                    port = Integer.parseInt(value);
+                                } catch (NumberFormatException e) {
+                                    throw new IllegalArgumentException("Invalid argument " + value + " for option " + key);
+                                }
+                                ++i;
+                            } else {
+                                throw new IllegalArgumentException("Option " + key + " requires an argument");
+                            }
+                            break;
+                        default:
+                            if (value != null && !value.startsWith("--")) {
+                                ++i;
+                            }
+                            throw new IllegalArgumentException("Unknown option " + key);
                     }
-                    ++i;
-                } else {
-                    throw new IllegalArgumentException(key);
                 }
             }
         } catch (IllegalArgumentException e) {
-            Logger.error("Unknown option: " + e.getMessage());
-            Logger.println("Usage: java teatime.server.TeaTimeServer [OPTION]...\n" +
-                    "Options:\n" +
-                    "\t--port\t\tlocal port number");
+            Logger.error(e.getMessage());
+            Logger.println(USAGE_STRING);
             System.exit(1);
         }
 
